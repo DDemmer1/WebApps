a215f8b62930f2137594ab884a5f4790adcbea52
Dennis <Dennis@172.20.151.208>
2017-05-27 03:58:37 +0200

    trying to implement article view

--- webapp17/src/main/java/de/uni_koeln/info/webapp/controller/HomeController.java
+++ webapp17/src/main/java/de/uni_koeln/info/webapp/controller/HomeController.java
@@ -2,28 +2,40 @@ package de.uni_koeln.info.webapp.controller;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.lucene.queryparser.classic.ParseException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import ch.qos.logback.core.Context;
 import de.uni_koeln.spinfo.textengineering.ir.lucene.Searcher;
 import de.uni_koeln.spinfo.textengineering.ir.model.IRDocument;
 
 @Controller
 public class HomeController {
+	
+	//Liste für die Ergebnisse der Suche
+	List<IRDocument> results;
 
+	
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "text/html")
 	public String defaultHandlerMethod(Model model) {
 		return "home";
 	}
 
+	
+	
 	@PostMapping(value = "/search")
 	public String getSearchResult(HttpServletRequest request, Model model) {
 
@@ -32,11 +44,11 @@ public class HomeController {
 			
 			String query = request.getParameter("query");
 
-			List<IRDocument> results = new ArrayList<>();
+			results = new ArrayList<>();
 			System.out.println(query);
 			
 			
-			results = searcher.search(query+"~",50);
+			results = searcher.search(query+"~",10);
 
 			model.addAttribute("results", results);
 			
@@ -51,17 +63,31 @@ public class HomeController {
 		return "home";
 	}
 	
-	@RequestMapping(value = "/article", method = RequestMethod.GET, produces = "text/html")
+	@GetMapping(value = "/article")
 	public String articleHandler(HttpServletRequest request, Model model) {
-		
-		String ird = request.getParameter("article");
-		
+		String uri = request.getParameter("article");
 		
 		
+		String text = getTextFromURI(uri);
 		
-//		model.addAttribute("text",text);
+		System.out.println(text);
+		
+		model.addAttribute("text",text);
 		
 		return "article";
+	}
+
+
+
+	private String getTextFromURI(String uri) {
+		
+		System.out.println("Size: "+results.size());
+		for(IRDocument ird : results){
+				if(uri.equals(ird.getURI())){
+				return ird.getContent();
+			}
+		}
+		return null;
 	}
 	
 
